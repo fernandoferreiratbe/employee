4c526abfee8ccf03827252de5c6c04f411a84b53
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2018-11-20 14:56:15 -0200

    Implement application domain

---
+++ src/main/java/io/github/fernandoferreiratbe/employees/domain/Developer.java
@@ -0,0 +1,13 @@
+package src.main.java.io.github.fernandoferreiratbe.employees.domain;
+
+public class Developer extends Employee {
+
+    public Developer(int id, String name, String surname, Address address, double salary) {
+        super(id, name, surname, address, salary);
+    }
+
+    @Override
+    public double getBonus() {
+        return super.getBonus() + 1200;
+    }
+}
