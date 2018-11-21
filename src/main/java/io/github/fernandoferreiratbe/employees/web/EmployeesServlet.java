ad157211993e1f50d7c4129162b0a4367de326fd
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2018-11-21 11:52:34 -0200

    Add employees servlet to the project

---
+++ src/main/java/io/github/fernandoferreiratbe/employees/web/EmployeesServlet.java
@@ -0,0 +1,24 @@
+package io.github.fernandoferreiratbe.employees.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/employees"} )
+public class EmployeesServlet extends HttpServlet {
+    private static final String DEFAULT_CONTENT_TYPE = "application/json";
+    private static final String DEFAULT_ENCONDING    = "utf-8";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
